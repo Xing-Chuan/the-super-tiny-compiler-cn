bc2898670eae963e8fbc88a40f6337b42c8a61b8
薛定谔的猫 <hh_2013@foxmail.com>
2017-01-11 06:18:45 +0800

    bugfix: filename error (#53)

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const {
   transformer,
   codeGenerator,
   compiler,
-} = require('./super-tiny-compiler');
+} = require('./the-super-tiny-compiler');
 const assert = require('assert');
 
 const input  = '(add 2 (subtract 4 2))';
